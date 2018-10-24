51c650acfe8c473ae33259883245fc210f66a9de
balasahebgulave <43171791+balasahebgulave@users.noreply.github.com>
2018-10-24 10:27:34 +0530

    Create mongodb_update_dict.py

---
+++ mongodb_update_dict.py
@@ -0,0 +1,15 @@
+
+#program for taking data from mongodb collection from that pick only updated data.
+
+
+transaction = db.feedbackData.find_one( {'$and':[{'bot_id':(botResponseInJson['botID'])},
+                                        {'user_details': d }]},{"chat_history":1} )
+                                        
+chat=[]
+
+for history in transaction["chat_history"]:
+  chat.append(history)
+  
+updated_transaction=(list({d['bot_response']:d for d in chat}.values()))
+
+print('-----------updated_transaction--------------',updated_transaction)
