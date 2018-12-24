935ce60858edd76972f5b45a1fcc9748acd3336b
balasahebgulave <43171791+balasahebgulave@users.noreply.github.com>
2018-12-24 12:26:26 +0530

    Update MongoDbgetUpdatedData.py

--- MongoDbgetUpdatedData.py
+++ MongoDbgetUpdatedData.py
@@ -1,5 +1,7 @@
 
+
 #program for taking data from mongodb collection from that pick only updated data.
+
 
 transaction = db.feedbackData.find_one( {'$and':[{'bot_id':(botResponseInJson['botID'])},
                                         {'user_details': d }]},{"chat_history":1} )
@@ -12,3 +14,6 @@ for history in transaction["chat_history"]:
 updated_transaction=(list({d['bot_response']:d for d in chat}.values()))
 
 print('updated_transaction : ', updated_transaction)
+
+
+
