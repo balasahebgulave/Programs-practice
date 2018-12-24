3912bf62710e15c001b0bd4182edd1a0c13aa01c
balasahebgulave <43171791+balasahebgulave@users.noreply.github.com>
2018-12-24 12:25:50 +0530

    Update and rename Mongo_Db_Update.py to MongoDbgetUpdatedData.py

--- MongoDbgetUpdatedData.py
+++ MongoDbgetUpdatedData.py
@@ -1,7 +1,6 @@
 
 #program for taking data from mongodb collection from that pick only updated data.
 
-
 transaction = db.feedbackData.find_one( {'$and':[{'bot_id':(botResponseInJson['botID'])},
                                         {'user_details': d }]},{"chat_history":1} )
                                         
@@ -12,4 +11,4 @@ for history in transaction["chat_history"]:
   
 updated_transaction=(list({d['bot_response']:d for d in chat}.values()))
 
-print('-----------updated_transaction--------------',updated_transaction)
+print('updated_transaction : ', updated_transaction)
