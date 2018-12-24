436b235d1d1e104c4b77fc1fe009b33da3332a71
balasahebgulave <43171791+balasahebgulave@users.noreply.github.com>
2018-12-24 12:21:07 +0530

    Create QuizForBOT.py

---
+++ QuizForBOT.py
@@ -0,0 +1,42 @@
+def getQuiz(botResponseInJson):
+    quizdata=db.Quiz.find_one({"bot_id" : ObjectId(botResponseInJson['botID'])})
+    questions=quizdata['questions']
+    qList = random.sample([i for i in range(len(questions))], 1)
+    qList = [questions[i] for i in qList]
+    botResponseInJson['qList']=qList
+
+
+def finalQuizResponse(botResponseInJson):
+
+    quizdata=db.Quiz.find_one({"bot_id" : ObjectId(botResponseInJson['botID'])})
+    questions=quizdata['questions']
+    Answers=[i["rightanswer"] for i in questions]
+
+
+    if  len(botResponseInJson['quick_reply'])==1:
+        
+        scoreCounter=int(botResponseInJson['score'])
+
+        if botResponseInJson['message'] in Answers:
+            scoreCounter+=1
+            botResponseInJson['score']=scoreCounter
+            
+        # print('----------------scoreCounter-----------',botResponseInJson['score'])
+
+        if botResponseInJson['quick_reply'][0]['quick_reply_intent']=="":
+            botResponseInJson['quick_reply'] = []
+            for score in str(botResponseInJson['score']):
+                botResponseInJson['bot_response']=('Your Score Is : '+ score)
+                botResponseInJson['score']=0
+                break
+
+        else:
+            botResponseInJson['bot_response']=botResponseInJson['qList'][0]['question']
+            reply=[]
+            q=botResponseInJson['qList'][0]  
+            for option in q['options']:
+                data={"quick_reply_type": "text", "text": "", "quick_reply_intent": ""}
+                data["text"]=q['options'][option]
+                data["quick_reply_intent"]=botResponseInJson['quick_reply'][0]['quick_reply_intent']
+                reply.append(data)
+            botResponseInJson['quick_reply']=reply
