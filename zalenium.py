f00580ea695a48cbc83d1f7f3d87bb551c9801ae
balasahebgulave <43171791+balasahebgulave@users.noreply.github.com>
2020-02-25 19:31:08 +0530

    Create zalenium.py

---
+++ zalenium.py
@@ -0,0 +1,31 @@
+import unittest
+from selenium import webdriver
+import requests, time
+import os
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
+
+
+try:
+    driver = webdriver.Remote(
+            command_executor='http://108.62.118.197:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.FIREFOX)   
+    driver.maximize_window()
+
+    driver.get('https://login.yahoo.com/')
+
+    username = driver.find_element_by_name('username')
+    username.send_keys('balasahebgulave')
+    username.send_keys(Keys.RETURN)
+    time.sleep(5)
+    password = driver.find_element_by_name('password')
+    password.send_keys('9766058622')
+    password.send_keys(Keys.RETURN)
+    time.sleep(5)
+    driver.quit()
+
+except Exception as e:
+    raise e
+
+
+
